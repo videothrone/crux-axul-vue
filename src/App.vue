378118009dbf0e5ed744593625c788be01dd60e5
Ferenc Petöcz <ferenc.petoecz@ressourcenmangel.de>
2023-01-24 17:44:00 +0100

    🚧 WIP

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ import Nav from '@/components/Nav.vue';
 
 <style lang="scss">
 @import '@/assets/base.scss';
+@import '@/assets/nav.scss';
 
 /* ANIMATION LOGO */
 @media (prefers-reduced-motion: no-preference) {
