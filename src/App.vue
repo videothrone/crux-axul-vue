02d6706ba85011c509cd241eb8e0a72ab180abd3
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-04-18 20:04:52 +0200

    💄 Adjust SCSS structure, variables and imports

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
-import Nav from '@/components/Nav.vue';
+import Nav from '@/components/nav/Nav.vue';
 </script>
 
 <template>
@@ -16,28 +16,3 @@ import Nav from '@/components/Nav.vue';
   </header>
   <div class="content"><RouterView /></div>
 </template>
-
-<style lang="scss">
-// @import '@/assets/base.scss';
-// @import '@/assets/release-detail.scss';
-
-/* ANIMATION LOGO */
-@media (prefers-reduced-motion: no-preference) {
-  .cxa-circle-logo {
-    animation: logo-spin infinite 20s linear;
-    animation-play-state: running;
-  }
-  .header__logo .cxa-circle-logo:hover {
-    animation-play-state: paused;
-  }
-}
-
-@keyframes logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-</style>
