b1ef7a445522e196b4e7e934453e8efd256a6451
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-04-22 16:30:41 +0200

    ♻️ Refactor Links /w proper markup + data

--- src/views/links/Links.vue
+++ src/views/links/Links.vue
@@ -1,64 +1,40 @@
 <template>
-  <div class="content__links">
-    <div class="content__links__link content__links-interview">
-      <a
-        href="http://www.fluxwebzine.it/en/interview-with-sankt-and-lymbs/"
-        rel="noreferrer noopener"
-        target="_blank"
-        >Label Interview</a
-      >
-    </div>
-    <hr class="hrr hrr__links" />
-    <div class="content__links__link">
-      <a
-        href="https://cruxaxul.bandcamp.com/"
-        rel="noreferrer noopener"
-        target="_blank"
-        >Buy Tapes & Digital (Bandcamp)</a
-      >
-    </div>
-    <div class="content__links__link">
-      <a
-        href="https://soundcloud.com/cruxaxul"
-        rel="noreferrer noopener"
-        target="_blank"
-        >Soundcloud</a
-      >
-    </div>
-    <div class="content__links__link">
-      <a
-        href="https://www.instagram.com/crux.axul/"
-        rel="noreferrer noopener"
-        target="_blank"
-        >Instagram</a
-      >
-    </div>
-    <div class="content__links__link">
-      <a
-        href="https://www.facebook.com/CruxAxulRec"
-        rel="noreferrer noopener"
-        target="_blank"
-        >Facebook</a
-      >
-    </div>
-    <div class="content__links__link">
-      <a
-        href="https://www.mixcloud.com/cruxaxul/"
-        rel="noreferrer noopener"
-        target="_blank"
-        >Mixes & Radioshows</a
-      >
-    </div>
-    <div class="content__links__link">
-      <a
-        href="https://www.youtube.com/channel/UCcKD1XMRMXVhO5wL29qiE4A"
-        rel="noreferrer noopener"
-        target="_blank"
-        >Youtube</a
-      >
-    </div>
-  </div>
+  <Loader v-if="isLoading"/>
+  <ul class="content__links" v-if="!isLoading">
+    <li v-for="link in links" :key="link.id" class="content__link" role="listitem">
+      <a :href="link.url" class="link link--inverted" rel="noreferrer noopener" target="_blank">{{ link.label }}</a>
+    </li>
+  </ul>
 </template>
+
+<script>
+import Loader from '@/components/loader/Loader.vue';
+import { fetchData } from '@/helpers/helperFunctions.js';
+import { ref, onMounted } from 'vue';
+
+export default {
+  components: {
+    Loader
+  },
+  setup() {
+    const links = ref([]);
+    const isLoading = ref(true);
+
+    onMounted(() => {
+      fetchData('/links.json')
+        .then(data => {
+          links.value = data;
+          isLoading.value = false;
+        })
+        .catch(err => console.log(err.message));
+    });
+
+    return {
+      links
+    };
+  }
+};
+</script>
 
 <style lang="scss">
 @import './Links.scss';
