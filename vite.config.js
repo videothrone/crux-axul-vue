d2a6e9c932aab5ad92121352bbbb3319705b9688
Ferenc <47146569+videothrone@users.noreply.github.com>
2025-03-21 19:38:22 +0100

    ⬆️ Upgrade dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,26 @@
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@use "@/assets/scss/main.scss" as *;`,
+        silenceDeprecations: ["import"],
+        quietDeps: true,
       },
     },
   },
   optimizeDeps: {
-    exclude: ['oh-vue-icons/icons'],
+    exclude: ["oh-vue-icons/icons"],
   },
 });
