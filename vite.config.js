77846bd6cb99dc65c52f79c7ab6beaa53b730dc9
Ferenc <47146569+videothrone@users.noreply.github.com>
2026-01-31 16:08:56 +0100

    ⚡️ Add Netlify caching/security headers & optimize build config

--- vite.config.js
+++ vite.config.js
@@ -22,10 +22,29 @@ export default defineConfig({
   },
   build: {
     cssCodeSplit: true,
+    cssMinify: true,
     minify: "terser",
     terserOptions: {
       compress: {
         drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ["console.log"],
+      },
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split(".");
+          const ext = info[info.length - 1];
+          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/i.test(assetInfo.name)) {
+            return `assets/images/[name]-[hash][extname]`;
+          } else if (/\.(woff2?|eot|ttf|otf)$/i.test(assetInfo.name)) {
+            return `assets/fonts/[name]-[hash][extname]`;
+          }
+          return `assets/${ext}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
       },
     },
   },
